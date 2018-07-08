e06963ca2157157bf270667c77a8554a32636671
evanc923 <evanc923@gmail.com>
2018-07-08 17:18:50 +0800

    views update

--- gambalcounter/ViewController.swift
+++ gambalcounter/ViewController.swift
@@ -11,24 +11,11 @@ import UIKit
 class ViewController: UIViewController{
 
     @IBOutlet weak var input_players: UITextField!
-    @IBOutlet weak var show_players: UILabel!
-    //var show_players: String!
+    @IBOutlet weak var currentPlayers: UILabel!
     
     
     @IBAction func add_players(_ sender: Any) {
-        show_players.text! = "Players in this game :" + input_players.text!
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        //input_players.delegate = self
-        // Do any additional setup after loading the view, typically from a nib.
-        //input_players.keyboardType = UIKeyboardType.numberPad
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
 
